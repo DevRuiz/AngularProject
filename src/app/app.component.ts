a54599f942fba456f6ddd866a4702730e0ec9457
Fernanda Ruiz <ferruiz200019@hotmail.com>
2020-03-22 00:33:11 -0600

    actualizacion de socket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {WebSocketService} from './services/web-socket.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cliente';
+  public wsService: WebSocketService;
+  constructor(wsService: WebSocketService){
+    this.wsService = wsService;
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  wsUrl: 'http://18.208.109.179:3000'
 };
 
 /*
